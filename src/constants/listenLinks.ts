f0cb3570ac599062bf3bfb795181fb24bf98a193
seantokuzo <seantokuzo@gmail.com>
2023-03-31 12:04:34 -0700

    add remaining music links

--- src/constants/listenLinks.ts
+++ src/constants/listenLinks.ts
@@ -1,19 +1,38 @@
+// TRACKS
+// when you call
+// paralyzed
+// all I know
+// watch you dance
+// weight
+
 export const spotifyEmbedLinks = [
   'https://open.spotify.com/embed/track/13Gps1CEaCGMIPo6C5OAmk?utm_source=generator',
-  'https://open.spotify.com/embed/track/1URSiykSUuDy3nc4Zr9UCN?utm_source=generator',
-  'https://open.spotify.com/embed/track/6oJh4YHmSgT4ofRR3x2V3Z?utm_source=generator',
   'https://open.spotify.com/embed/track/7DWfy5VhmRZaSUr3GEkA4S?utm_source=generator',
-  'https://open.spotify.com/embed/track/3mSLcxhDXVJYXwHByTlz6g?utm_source=generator'
+  'https://open.spotify.com/embed/track/1URSiykSUuDy3nc4Zr9UCN?utm_source=generator',
+  'https://open.spotify.com/embed/track/3mSLcxhDXVJYXwHByTlz6g?utm_source=generator',
+  'https://open.spotify.com/embed/track/6oJh4YHmSgT4ofRR3x2V3Z?utm_source=generator'
 ]
 
 export const appleMusicEmbedLinks = [
-  'https://embed.music.apple.com/us/album/watch-you-dance/1219609224?i=1219609231'
+  'https://embed.music.apple.com/us/album/when-you-call/1578077112?i=1578077114',
+  'https://embed.music.apple.com/us/album/paralyzed/1578063176?i=1578063177',
+  'https://embed.music.apple.com/us/album/all-i-know/1219609224?i=1219609235',
+  'https://embed.music.apple.com/us/album/watch-you-dance/1219609224?i=1219609231',
+  'https://embed.music.apple.com/us/album/weight/1214938030?i=1214938102'
 ]
 
 export const amazonMusicEmbedLinks = [
-  'https://music.amazon.com/embed/B09B4XWG17/?id=Zg1Jj2b9il&marketplaceId=ATVPDKIKX0DER&musicTerritory=US'
+  'https://music.amazon.com/embed/B09B4WWYK2/?id=IWZ9CPaFXc&marketplaceId=ATVPDKIKX0DER&musicTerritory=US',
+  'https://music.amazon.com/embed/B09B4XWG17/?id=Zg1Jj2b9il&marketplaceId=ATVPDKIKX0DER&musicTerritory=US',
+  'https://music.amazon.com/embed/B06XVJ4812/?id=NuVEDvZuAK&marketplaceId=ATVPDKIKX0DER&musicTerritory=US',
+  'https://music.amazon.com/embed/B06XVCBJMN/?id=tXBaYMXlz3&marketplaceId=ATVPDKIKX0DER&musicTerritory=US',
+  'https://music.amazon.com/embed/B06XK2X2JN/?id=pyl3DlPfHp&marketplaceId=ATVPDKIKX0DER&musicTerritory=US'
 ]
 
 export const soundCloudEmbedLinks = [
-  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/828635404&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1093235116&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1093220461&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/323515283&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/314219112&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/312091674&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
 ]
