924f4b04ccf129bd3233d14c9a64c45922bfd5ce
seantokuzo <seantokuzo@gmail.com>
2023-03-31 14:58:53 -0700

    remove logo img from smallest screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,20 +23,18 @@ const Header = () => {
 
   return (
     <header
-      className={`w-full absolute top-0 py-5 z-50
-      bg-jpBg
-      flex justify-between items-center`}
+      className={`w-full absolute top-0 py-5
+      z-50 bg-jpBg
+      flex ${windowSize.width < 360 ? 'justify-center' : 'justify-between'} items-center`}
     >
-      {windowSize.width < 640 && (
+      {windowSize.width < 640 && windowSize.width >= 360 && (
         <div
           className="w-12 h-12 ml-4 rounded-full bg-cover bg-no-repeat bg-center text-xxs flex justify-center items-center"
           style={{
             backgroundImage:
               'url(https://static.wixstatic.com/media/425622_9e005c72ce634b4f8c991c2648ad0f4d~mv2.jpeg/v1/fill/w_1335,h_943,fp_0.50_0.47,q_85,usm_0.66_1.00_0.01,enc_auto/425622_9e005c72ce634b4f8c991c2648ad0f4d~mv2.jpeg)'
           }}
-        >
-          LOGO
-        </div>
+        ></div>
       )}
       {windowSize.width >= 640 && <h1 className="text-4xl ml-8">JORDAN PARKER</h1>}
       <div className="w-fit flex justify-end items-center pr-4 md:pr-6">
