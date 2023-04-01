1ce3c9b5c14d9cdc2a25fcf0aaf527aa15e20915
seantokuzo <seantokuzo@gmail.com>
2023-04-01 14:33:29 -0700

    conditionally absolute footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,14 @@ const Footer = () => {
 
   return (
     <footer
-      className={`w-full py-4 lg:py-6
+      className={`w-full py-4 lg:py-6 ${
+        pathname === '/contact' && windowSize.height > 700 && 'absolute bottom-1'
+      }
       flex flex-col justify-center items-center`}
     >
-      {pathname !== '/' && <SocialLinks />}
+      <div className={`${windowSize.width < 420 ? 'w-[250px]' : ''}`}>
+        {pathname !== '/' && <SocialLinks />}
+      </div>
       <p className="mt-6 text-xxs sm:text-xs">
         Copyright © 2023 · All Rights Reserved · Jordan Parker
       </p>
