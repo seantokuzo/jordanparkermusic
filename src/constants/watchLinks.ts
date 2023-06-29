ca5674cc1d480ba4b33d21174b32e950db1ddaa2
seantokuzo <seantokuzo@gmail.com>
2023-06-28 23:55:00 -0700

    add new jp youtube video

--- src/constants/watchLinks.ts
+++ src/constants/watchLinks.ts
@@ -1,4 +1,5 @@
 export const watchLinks = [
+  'https://www.youtube.com/embed/kvNjIfgYzR0',
   'https://www.youtube.com/embed/PE44II_Vq4U',
   'https://www.youtube.com/embed/ERNAVdDhOlA',
   'https://www.youtube.com/embed/erDc3X8f4kE',
